6848282b2ae852ba5aad870bf19d7d74831fd8ec
wxjaa <980607279@qq.com>
2017-03-31 17:59:53 +0800

    webpack 打包路径 修改  图片打包路径还是有问题

--- build/utils.js
+++ build/utils.js
@@ -6,7 +6,7 @@ exports.assetsPath = function (_path) {
   var assetsSubDirectory = process.env.NODE_ENV === 'production'
     ? config.build.assetsSubDirectory
     : config.dev.assetsSubDirectory
-  return path.posix.join('../',assetsSubDirectory, _path)
+  return path.posix.join(assetsSubDirectory, _path)
 }
 
 exports.cssLoaders = function (options) {
